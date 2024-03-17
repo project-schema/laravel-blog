8930394061ca6ffc9e88ad1d333e433a99a4da05
abdur-shobur <abdurshobur.developer@gmail.com>
2024-03-17 07:09:43 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.layout')
+@section('title', 'Home Dx-Blog')
+
+@section('container')
+
+    @include('components.search')
+
+    <div class="row tm-row">
+        @include('components.article')
+        @include('components.article')
+        @include('components.article')
+        @include('components.article')
+        @include('components.article')
+    </div>
+
+    @include('components.pagination')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,29 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'home' );
+} );
+Route::get( '/blog-details', function () {
+    return view( 'single_post' );
+} );
+Route::get( '/about-us', function () {
+    return view( 'about_us' );
+} );
+Route::get( '/contact-us', function () {
+    return view( 'contact_us' );
+} );
+// Route::get( '/', function () {
+//     return view( 'home' )->name( 'home' );
+// } );
+// Route::get( '/blog-details', function () {
+//     return view( 'single_post' )->name( 'blog-details' );
+// } );
+// Route::get( '/about-us', function () {
+//     return view( 'about_us' )->name( 'about-us' );
+// } );
+// Route::get( '/contact-us', function () {
+//     return view( 'contact_us' )->name( 'contact-us' );
+// } );
